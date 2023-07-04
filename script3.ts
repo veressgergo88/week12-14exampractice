2ac4898e7f4973938fd6ec1d78958dd26f583362
veressgergo88 <veress.gergo88@gmail.com>
2023-07-04 23:23:43 +0200

    script3 fixed

--- script3.ts
+++ script3.ts
@@ -4,44 +4,39 @@ type Clone = {
   gender: string;
 };
 
-let clonePerson1 = [
-  {
+let clonePerson1 = {
     name: "Boba Fett",
     age: 5,
     gender: "male",
-  },
-];
+  }
 
-let clonePerson2 = [
+let clonePerson2 = 
   {
     name: "Princess Amidala",
     age: 25,
     gender: "female",
-  },
-];
+  }
 
-let clonePerson3 = [
+let clonePerson3 = 
   {
     name: "Obi-Wan Kenobi",
     age: 40,
     gender: "male",
-  },
-];
+  }
 
-function createCloneArmy(person: Clone[], count: number): Clone[] {
+function createCloneArmy(person: Clone, count: number): Clone[] {
   let result: Clone[] = [];
 
   let i = 0;
-  let y = 0;
   if (count <= 100) {
-    while (y !== count) {
-      result = [...result, person[i]];
-      y++;
+    while (i !== count) {
+      result = [...result, person];
+      i++;
     }
   } else {
-    while (y !== 100) {
-      result = [...result, person[i]];
-      y++;
+    while (i !== 100) {
+      result = [...result, person];
+      i++;
     }
   }
 
